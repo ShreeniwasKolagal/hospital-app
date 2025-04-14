511e36e71ed37f40fc951c71ce9ab1cecd6cfed7
ShreeniwasKolagal <kolagalshreeniwas@gmail.com>
2025-04-14 20:57:12 +0530

    update db.js for deployment on railway

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+const url = require('url');
+
+const dbUrl = new URL(process.env.DATABASE_URL)
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: dbUrl.hostname,
+    port: dbUrl.port,
+    user: dbUrl.username,
+    password: dbUrl.password,
+    database: 'railway',
 });
 
 db.connect(err => {
